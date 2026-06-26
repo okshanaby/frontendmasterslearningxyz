81bd59253de8a74c089312da162804af017b6a92
okshanaby <steamnature006@gmail.com>
2026-06-26 16:27:53 +0300

    Using WebSockets with Express

--- index-ws.js
+++ index-ws.js
@@ -9,4 +9,41 @@ app.get("/", (req, res)=> {
 
 server.on("request", app)
 
-server.listen(3001, ()=> console.log("Websocket Server running on port 3001"))
+server.listen(3000, ()=> console.log("Websocket Server running on port 3000"))
+
+/** Begin Websocket Server Code */
+
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({server})
+
+// Custom broadcast function
+// Added a check to ensure we only send to OPEN clients
+wss.broadcast = (data) => {
+    wss.clients.forEach(client => {
+        if (client.readyState === client.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+
+wss.on("connection", (ws)=> {
+	const numOfClients = wss.clients.size
+	console.log("Connected clients: ", numOfClients)
+
+	wss.broadcast(`Current Visitors: ${numOfClients}`);
+
+	// ON CLIENT OPEN 
+	if(ws.readyState === ws.OPEN) {
+		ws.send("Welcome to my server")
+	}
+
+	// ON CLIENT CLOSE 
+	ws.on("close", ()=> {
+		// Recalculate the actual size AFTER the disconnect
+        const currentCount = wss.clients.size;
+        wss.broadcast(`Current Visitors: ${currentCount}`);
+        console.log("Client has disconnected. Remaining: ", currentCount);
+	})
+})
